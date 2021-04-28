2a17bfac128c4654f38164345b965411a2db007e
XGSClear7 <17615800805@163.com>
2021-04-28 10:32:32 +0800

    20210428 10:32

--- other/FileClassification.py
+++ other/FileClassification.py
@@ -4,18 +4,21 @@ import shutil
 from tqdm import tqdm
 
 # 文件目录
-path = r'F:\MyProjects\spider_data\sopei\sopei'
+path = r'E:\spider_data\imgs'
 # 结果存放目录
-Data1 = r'F:\MyProjects\spider_data\sopei\高清图'
-Data2 = r'F:\MyProjects\spider_data\sopei\缩略图'
+Data1 = r'E:\spider_data\高清图'
+Data2 = r'E:\spider_data\缩略图'
 # 读取文件列表
 files_list = os.listdir(path)
 
 for file in tqdm(files_list):
     filename, suffix = os.path.splitext(file)  # filename是文件名 suffix是文件后缀
+    # print(filename)
     if '&type=thumbnail' not in filename:  # 判断条件
         file_path = os.path.join(path, filename + suffix)  # 文件路径
+        # print(file_path)
         shutil.copy(file_path, Data1)
     else:
-        file_path = os.path.join(path, filename.replace('&type=thumbnail', '') + suffix)  # 文件路径
+        file_path = os.path.join(path, filename + suffix)  # 文件路径
+        # print(file_path)
         shutil.copy(file_path, Data2)
